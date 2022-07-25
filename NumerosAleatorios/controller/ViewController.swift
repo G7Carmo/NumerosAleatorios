d72cf34ad1e45b7df121ffaa10fd420ed8177354
Gds Desenvolvimento e Tecnologia <104175823+GdsDesenvolvimentoETecnologia@users.noreply.github.com>
2022-07-24 22:52:55 -0300

    finish

--- NumerosAleatorios/controller/ViewController.swift
+++ NumerosAleatorios/controller/ViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    @IBAction func gerarRandonNumber(_ sender: Any) {
+        let numero = arc4random_uniform(11)
+        legendaResultado.text = String(numero)
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
